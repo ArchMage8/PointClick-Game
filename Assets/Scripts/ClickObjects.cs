176885f7ab062066892b7e284cffba1089ff94eb
Arvin Sadeli <darryl.orleans@binus.ac.id>
2024-08-08 16:26:40 +0700

    Setup

---
+++ Assets/Scripts/ClickObjects.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickObjects : MonoBehaviour
+{
+
+    public bool CanClick = true;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && CanClick)
+        {
+            DetectClickedObject();
+        }
+    }
+
+    void DetectClickedObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        if (hit.collider != null)
+        {
+            GameObject clickedObject = hit.collider.gameObject;
+            ClickHandler(clickedObject);
+        }
+    }
+
+    private void ClickHandler(GameObject ClickedObject)
+    {
+        
+    }
+}
