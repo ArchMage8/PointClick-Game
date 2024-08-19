81e6a1380a3a9fff243cb873c282dd1228b9b68d
Arvin Sadeli <darryl.orleans@binus.ac.id>
2024-08-19 10:28:23 +0700

    added cam limits

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraMover : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    [SerializeField] private int maxLeft;
+    [SerializeField] private int maxRight;
 
     private bool isMovingLeft = false;
     private bool isMovingRight = false;
@@ -43,11 +45,18 @@ public class CameraMover : MonoBehaviour
 
     private void MoveLeft()
     {
-        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if (transform.position.x > maxLeft)
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
     }
 
     private void MoveRight()
     {
-        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        if (transform.position.x < maxRight)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+        }
     }
 }
