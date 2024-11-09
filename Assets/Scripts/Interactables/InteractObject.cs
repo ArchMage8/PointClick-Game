b1ca5a3eaf546848b8e6c5ada69a95d07dcc1f81
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-11-09 18:29:19 +0700

    Update InteractObject.cs

--- Assets/Scripts/Interactables/InteractObject.cs
+++ Assets/Scripts/Interactables/InteractObject.cs
@@ -105,10 +105,8 @@ public class InteractObject : MonoBehaviour
 
         TextFade.SetActive(true);
         clickObjects.CanClick = false;
-       
-       
      
-        StartCoroutine(dialogueController.WriteSentence());
+        dialogueController.NextSentence();
         hasBeenInteractedHolder.HasBeenInteracted = true;
     }
 
